040b4e511a566b049331038a82c02df85df4aa7d
Christopher Fowler <chaydenfowler@gmail.com>
2022-03-08 13:36:00 +1300

    Q17 Minor comments

--- 2021/Q17/Q17.1_chris.py
+++ 2021/Q17/Q17.1_chris.py
@@ -20,7 +20,8 @@ print(xmin, xmax, ymin, ymax)
 # Triangular number = x*(x+1)/2
 # Technically incorrect solution, as x_pos is difference between 2 triangular numbers.
 # We just assume x_pos = tri(x_init) - tri(0) and get easy solution.
-# Breaks when x_init > 2*y_init+1 
+# Can break when x_init > 2*y_init+1 
+# 100% breaks when xmin..xmax does not contain any triangular numbers.
 max_speed = max(abs(int(ymin)), abs(int(ymax)))-1
 print(max_speed)
 max_height = max_speed*(max_speed+1)/2
