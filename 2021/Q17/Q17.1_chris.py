5667efa264bec0293fed39ea140df6453e47f1c2
Christopher Fowler <chaydenfowler@gmail.com>
2022-03-08 13:31:32 +1300

    Minor comment to Q17

--- 2021/Q17/Q17.1_chris.py
+++ 2021/Q17/Q17.1_chris.py
@@ -18,8 +18,9 @@ ymin, ymax = input_text.split(',')[1].split('=')[1].split('..')
 print(xmin, xmax, ymin, ymax)
 
 # Triangular number = x*(x+1)/2
-# Technically incorrect solution, as x_pos is different between 2 triangular numbers.
+# Technically incorrect solution, as x_pos is difference between 2 triangular numbers.
 # We just assume x_pos = tri(x_init) - tri(0) and get easy solution.
+# Breaks when x_init > 2*y_init+1 
 max_speed = max(abs(int(ymin)), abs(int(ymax)))-1
 print(max_speed)
 max_height = max_speed*(max_speed+1)/2
