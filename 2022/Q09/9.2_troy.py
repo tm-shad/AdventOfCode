9fddb1fa481ca4c4997e38b83e939480a8de1d81
Troy Murray <mail@troymurray.dev>
2022-12-11 12:06:11 +1100

    Troy: 2022 Added git output to Q9

--- 2022/Q09/9.2_troy.py
+++ 2022/Q09/9.2_troy.py
@@ -1,15 +1,11 @@
 from pathlib import Path
 import logging
 import argparse
-from typing import List, Tuple
+from typing import Dict, List, Tuple
 
-import numpy as np
+from PIL import Image
 
-from pprint import pprint
-
-import itertools
-
-from math import ceil, floor
+from tqdm import tqdm
 
 
 def read_file(file_path: Path) -> str:
@@ -25,11 +21,12 @@ class Point:
         self.x = x
         self.y = y
         self.visited = dict()
+        self.round_visited = dict()
         self.last_pos = (x, y)
 
-        self.update_visited()
+        self.update_visited(0)
 
-    def update_point(self, x: int, y: int):
+    def update_point(self, x: int, y: int, curr_round: int):
         # update last pos
         self.last_pos = (self.x, self.y)
 
@@ -38,15 +35,16 @@ class Point:
         self.y = y
 
         # update visited dict
-        self.update_visited()
+        self.update_visited(curr_round)
 
-    def update_visited(self):
+    def update_visited(self, curr_round: int):
         self.visited[(self.x, self.y)] = True
+        self.round_visited[curr_round] = (self.x, self.y)
 
-    def move_point(self, move_tpl):
-        self.update_point(self.x + move_tpl[0], self.y + move_tpl[1])
+    def move_point(self, move_tpl, curr_round: int):
+        self.update_point(self.x + move_tpl[0], self.y + move_tpl[1], curr_round)
 
-    def follow(self, other):
+    def follow(self, other, curr_round: int):
         if self.get_distance(other) > 1:
             # directly x change
             if self.y == other.y and self.x != other.x:
@@ -59,9 +57,9 @@ class Point:
                 dx = -(self.x - other.x)
                 dy = -(self.y - other.y)
 
-                self.move_point((dx // abs(dx), dy // abs(dy)))
+                self.move_point((dx // abs(dx), dy // abs(dy)), curr_round)
 
-        self.update_visited()
+        self.update_visited(curr_round)
 
     def get_distance(self, other):
         dx = abs(other.x - self.x)
@@ -70,24 +68,82 @@ class Point:
         return max([dx, dy])
 
 
-def show_board(head: Point, tails: List[Point], w=6, h=5):
-    board = [["." for x in range(w)] for y in range(h)]
+IMAGE_RESIZE = 40
 
-    # for pos, _ in tail.visited.items():
-    #     x, y = pos
-    #     board[y][x] = "#"
+def create_board_image(
+    w: int,
+    h: int,
+    x_padding: int,
+    y_padding: int,
+    nodes: Dict[str, Tuple[int, int]],
+    tail_history: List[Tuple[int, int]],
+):
+    image = Image.new("RGB", [w + x_padding, h + y_padding])
 
-    board[0][0] = "s"
+    for tx, ty in tail_history:
+        color = (0, 0, 100)
+        x = tx + x_padding
+        y = ty + y_padding
+        image.putpixel((x, y), color)
 
-    for i in range(len(tails)):
-        tail = tails[i]
-        board[tail.y][tail.x] = str(i + 1)
-    board[head.y][head.x] = "H"
+    for k, n in nodes.items():
+        color = (255, 255, 255)
+        if k == "H":
+            color = (0, 255, 0)
+        if k == str(len(nodes) - 1):
+            color = (0, 0, 255)
+        x = n[0] + x_padding
+        y = n[1] + y_padding
+        image.putpixel((x, y), color)
 
-    print("\n")
-    board.reverse()
-    for i in board:
-        print("".join(i))
+    return image
+
+
+def create_gif(head: Point, tails: List[Point], frames: List[int]):
+    # get maximum width of the board
+    w = max(x for x, _ in head.visited.keys()) + 1
+    h = max(y for _, y in head.visited.keys()) + 1
+
+    # the x/y coords can go negative, so get the ammount of extra padding needed to avoid an out of range exception
+    x_padding = abs(min(x for x, _ in head.visited.keys()))
+    y_padding = abs(min(y for _, y in head.visited.keys()))
+
+    # create an empty list of image frames
+    images = []
+
+    for frame_i in tqdm(frames):
+        nodes = dict()
+        nodes["H"] = head.round_visited[frame_i]
+        for j in range(len(tails)):
+            nodes[str(j + 1)] = tails[j].round_visited[frame_i]
+
+        images.append(
+            create_board_image(
+                w,
+                h,
+                x_padding,
+                y_padding,
+                nodes,
+                [tpl for k, tpl in tails[-1].round_visited.items() if k <= frame_i],
+            )
+        )
+
+    image = Image.new("RGB", [w + x_padding, h + y_padding]).resize(
+        [w * IMAGE_RESIZE, h * IMAGE_RESIZE], Image.NEAREST
+    )
+    image.save(
+        "test.gif",
+        save_all=True,
+        append_images=(
+            i.resize([w * IMAGE_RESIZE, h * IMAGE_RESIZE], Image.NEAREST)
+            for i in images
+        ),
+        # duration=10,
+        duration=len(images) // 2,
+        loop=0,
+    )
+
+    pass
 
 
 def main(input_path: Path):
@@ -100,21 +156,24 @@ def main(input_path: Path):
         "R": (1, 0),
     }
     head = Point(0, 0)
-    tails = [Point(0, 0) for i in range(1)]
+    tails = [Point(0, 0) for i in range(9)]
+
+    curr_round = 0
+    major_frames = []
 
     for line in in_str.splitlines():
         d, l = line.split(" ")
 
         for i in range(int(l)):
-            head.move_point(movement_dict[d])
-            tails[0].follow(head)
+            head.move_point(movement_dict[d], curr_round=curr_round)
+            tails[0].follow(head, curr_round=curr_round)
 
             for i in range(len(tails) - 1):
-                tails[i + 1].follow(tails[i])
+                tails[i + 1].follow(tails[i], curr_round=curr_round)
+            major_frames.append(curr_round)
+            curr_round += 1
 
-            # show_board(head, tails)
-            pass
-
+    create_gif(head, tails, major_frames)
     return len(tails[-1].visited)
 
 
