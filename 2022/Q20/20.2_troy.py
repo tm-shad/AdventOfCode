d12788f00dbed0af5eda58e828659e350d31831f
Troy <troysmurray@gmail.com>
2022-12-20 13:58:10 +0800

    Troy: 2022Q20

---
+++ 2022/Q20/20.2_troy.py
@@ -0,0 +1,108 @@
+from ast import Dict, List, Set
+import copy
+from functools import reduce
+from pathlib import Path
+import logging
+import argparse
+from typing import Any, Iterator
+
+import networkx as nx
+
+from tqdm import tqdm
+
+import operator
+import re
+
+from ortools.sat.python import cp_model
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def read_file(file_path: Path) -> str:
+
+    with open(file_path) as f:
+        in_str = f.read()
+
+    return in_str
+
+class Val:
+    def __init__(self, v) -> None:
+        self.value = v
+    
+    def make_movement(self, parent_list: list):
+        index = parent_list.index(self)
+
+        # pop
+        parent_list.pop(index)
+        new_index = (self.value+index)%len(parent_list)
+
+        
+        # logging.debug(f"{self.value} moves between {left} and {right}:")
+
+
+        # insert
+        parent_list.insert(new_index, self)
+
+
+        # logging.debug(", ".join(str(v.value) for v in parent_list) + "\n")
+    
+    def __repr__(self) -> str:
+        return f"Val({self.value})"
+
+E_KEY = 811589153
+def main(input_path: Path):
+    in_str = read_file(input_path).strip()
+
+    orig_list = [Val(int(s)*E_KEY) for s in in_str.splitlines()]
+
+    # for v in orig_list:
+    #     v.value = (v.value)#%len(orig_list)
+
+    zero_idx = [i for i,v in enumerate(orig_list) if v.value==0][0]
+    mixed_list = []
+    for i,v in tqdm(enumerate(orig_list)):
+        mixed_list.insert(i, v)
+
+    
+    logging.debug("Initial arrangement:")
+    logging.debug(", ".join(str(v.value) for v in mixed_list) + "\n")
+
+    for _ in range(10):
+        for v in orig_list:
+            v.make_movement(mixed_list)
+
+    logging.debug("End arrangement:")
+    logging.debug(", ".join(str(v.value) for v in mixed_list) + "\n")
+
+    i = mixed_list.index(orig_list[zero_idx])
+    coords = []
+    coords.append(mixed_list[(i+1000)%len(mixed_list)].value)
+    coords.append(mixed_list[(i+2000)%len(mixed_list)].value)
+    coords.append(mixed_list[(i+3000)%len(mixed_list)].value)
+
+
+    # def show_list():
+    #     return [vals[j] for j in rev_ptrs]
+    # logging.debug("Initial arrangement:")
+    # logging.debug(str(show_list()) + "\n")
+    # for i, val in enumerate(vals):
+    #     orig_ptr = ptrs
+
+
+
+    logging.debug("Final Coords:")
+    logging.debug(str(coords) + "\n")
+    return sum(coords)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file")
+
+    args = parser.parse_args()
+
+    input_file = str(Path(__file__).parent.joinpath(args.file).resolve())
+
+    print(main(input_file))
